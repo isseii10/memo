93fe363887030cc789643db308ef51f4fad8c30d
isseii10 <t1l.g2.o11g@gmail.com>
2024-03-23 23:50:56 +0900

    login, sign up

---
+++ src/components/Button.tsx
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+interface Props {
+  label: string
+}
+const Button = ({ label }: Props): JSX.Element => {
+  return (
+    <View style={styles.button}>
+      <Text style={styles.buttonLabel}>{label}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#234123',
+    borderRadius: 4,
+    alignSelf: 'flex-start',
+    marginBottom: 24
+  },
+  buttonLabel: {
+    fontSize: 16,
+    lineHeight: 32,
+    color: '#ffffff',
+    paddingVertical: 8,
+    paddingHorizontal: 24
+  }
+})
+
+export default Button
