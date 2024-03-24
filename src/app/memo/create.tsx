2b9dac769737cbc8fa61e2e2728b7eb0899f4208
isseii10 <t1l.g2.o11g@gmail.com>
2024-03-24 19:07:23 +0900

    メモ内容を保存する

--- src/app/memo/create.tsx
+++ src/app/memo/create.tsx
@@ -1,23 +1,46 @@
-import { KeyboardAvoidingView, StyleSheet, TextInput, View } from 'react-native'
+import { StyleSheet, TextInput, View } from 'react-native'
 import CircleButton from '../../components/CircleButton'
 import { Feather } from '@expo/vector-icons'
 import { router } from 'expo-router'
+import { Timestamp, addDoc, collection } from 'firebase/firestore'
+import { auth, db } from '../../config'
+import { useState } from 'react'
+import KeyboardSafeView from '../../components/KeyboardSafeView'
 
-const handlePress = (): void => {
-  router.back()
+const handlePress = (bodyText: string): void => {
+  if (auth.currentUser === null) { return }
+  const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
+  addDoc(ref, {
+    bodyText, // keyとvalueの変数名が同じなら省略できる
+    updatedAt: Timestamp.fromDate(new Date())
+  })
+    .then((docRef) => {
+      console.log('success', docRef.id)
+      router.back()
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 }
 
 const Create = (): JSX.Element => {
+  const [bodyText, setBodyText] = useState('')
   return (
-    <KeyboardAvoidingView behavior='height' style={styles.container}>
+    <KeyboardSafeView style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput multiline style={styles.input} value='' />
+        <TextInput
+          multiline
+          style={styles.input}
+          value={bodyText}
+          onChangeText={(text) => { setBodyText(text) }}
+          autoFocus
+        />
       </View>
-      <CircleButton onPress={handlePress}>
+      <CircleButton onPress={() => { handlePress(bodyText) }}>
         < Feather name='check' size={40} />
       </CircleButton>
 
-    </KeyboardAvoidingView >
+    </KeyboardSafeView >
   )
 }
 
